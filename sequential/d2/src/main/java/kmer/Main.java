0e70858dd752b9df5534f217f5c3d16ed43f97d0
Mariangela Petraglia <40724691+MaryPet91@users.noreply.github.com>
2018-07-09 15:28:07 +0200

    Fix d2 formula

--- sequential/d2/src/main/java/kmer/Main.java
+++ sequential/d2/src/main/java/kmer/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		D2 d2 = new D2(2);	
-		String Q="ATCGGCTGACCCGT";
-		String S="CTGAATGCATGGCA";
+		D2 d2 = new D2(15);	
+		long starTime = System.currentTimeMillis();
+		String Q="ATCGGCTGACCCGTA";
+		//String S="ATCGGCTGACCCGTA";
+		String S="GCTGAATGCATGGCA";
 		long score = d2.score(S, Q);
+		long endTime = System.currentTimeMillis();
+		double totTime = (double)(endTime - starTime)/1000;
 		System.out.println("Score di Q e S = "+score);
+		System.out.println("Tempo totale: "+totTime+ " sec");
 		
 	}
 
