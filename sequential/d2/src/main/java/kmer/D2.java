427319b6d930cd4a00ee25223b9f45afd495ba7f
Mariangela Petraglia <40724691+MaryPet91@users.noreply.github.com>
2018-07-10 16:18:37 +0200

    Fix add score normalized

--- sequential/d2/src/main/java/kmer/D2.java
+++ sequential/d2/src/main/java/kmer/D2.java
@@ -11,35 +11,55 @@ public class D2 {
 
 	private int k_len;
 	
+	
 	public D2(int k_len) {
 		this.k_len = k_len;
 	}
 	
 	/**
 	 * Calcola la somiglianza delle stringhe secondo l'algoritmo D2
-	 * @param S
-	 * @param Q
+	 * @param S sequenza genomica
+	 * @param Q sequenza genomica
 	 * @return score
 	 */
 	public long score(String S, String Q){
 		long score=0,Si=0,Qi=0;
 		String kmer="";
+		// D2(S,Q) = Σ(si - qi)^2, i=1 to n^k
 		for(int i = 0; i<k_len; i++) {
 			for(int j = 0; j<Math.pow(4, i); j++) {
 				kmer = nextKmer(i,j);
 				Si = occurrance(S, kmer);
 				Qi = occurrance(Q, kmer);
-				score += Math.pow((Si - Qi), 2);
+				score += Si*Qi;
 			}
 		}
 		return score;
 	}
 	
 	/**
+	 * Calcola la somiglianza delle stringhe secondo l'algoritmo D2 normalizzato
+	 * @param A sequenza genomica
+	 * @param B sequenza genomica
+	 * @return scoreNorm
+	 */
+	public double scoreNormalized(String A,String B){
+		double scoreNorm=0;
+		int S=0;
+		double radical2 = Math.sqrt(2);
+		long sigmaAA = score(A,A);
+		long sigmaBB = score(B,B);
+		double distanceEucl = Math.sqrt(sigmaAA*sigmaAA+sigmaBB*sigmaBB);
+		// D2 = −ln(S/(√2 * Σ(A,A) * Σ(B,B)/√((Σ(A,A))2 + (Σ(B,B))2))).
+		scoreNorm = - Math.log(S/(radical2*sigmaAA*sigmaBB/distanceEucl));
+		return scoreNorm;
+	}
+	
+	/**
 	 * Conta il numero di occorrenze del kmero kmer all'interno della stringa X
-	 * @param x
-	 * @param kmer
-	 * @return occurrance
+	 * @param x sequenza genomica
+	 * @param kmer k-mero da ricercare
+	 * @return occurrance occorrenza del kmer
 	 */
 	private long occurrance(String x, String kmer){
 		long result = 0;
@@ -54,9 +74,9 @@ public class D2 {
 	/**
 	 * Ottieni il k-mero in base al prossimo indice rankIndex, che definisce il rank 
 	 * in orgine lessicografico del k-mero
-	 * @param klength
-	 * @param rankIndex
-	 * @return nextKmer
+	 * @param klength lunghezza k-mero
+	 * @param rankIndex rank lessicografico del k-mero
+	 * @return nextKmer prossimo k-mero
 	 */
 	private String nextKmer(int klength,int rankIndex){
 		String s = this.convert(rankIndex);
@@ -66,13 +86,12 @@ public class D2 {
 		}
 		return s;
 	}
-	
 
 	/**
 	 * Ottieni il k-mero in base al prossimo indice rankIndex, che definisce il rank 
 	 * in orgine lessicografico del k-mero.
 	 * Quindi si converte l'indice lessicografico decimale in base-4 per poi codificarlo nell'alfabeto sigma {A,C,G,T}
-	 * L'implentazione � basato su toString(x, radix) di Integer.
+	 * L'implentazione è basato su toString(x, radix) di Integer.
 	 * @param i
 	 * @return string convert 
 	 */
