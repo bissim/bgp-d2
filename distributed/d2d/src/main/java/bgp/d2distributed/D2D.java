718a18ea96726cc5d135b81078f412363308fee9
Simone B <s.bisogno10@studenti.unisa.it>
2018-07-19 10:54:56 +0200

    Remote unnecessary files
    Eclipse file project have been removed and added to .gitignore files.
    
    Eh Sergio?

--- distributed/d2d/src/main/java/bgp/d2distributed/D2D.java
+++ distributed/d2d/src/main/java/bgp/d2distributed/D2D.java
@@ -93,6 +93,7 @@ public class D2D {
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
+
 	}
 
 }
