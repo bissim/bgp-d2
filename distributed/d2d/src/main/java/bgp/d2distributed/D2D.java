4d1c86c84baefd39e940a4e3dc735bfe8599e70a
Mariangela Petraglia <40724691+MaryPet91@users.noreply.github.com>
2018-07-19 18:54:09 +0200

    Add class LineInputFormat, modify Mapper plus setup method

--- distributed/d2d/src/main/java/bgp/d2distributed/D2D.java
+++ distributed/d2d/src/main/java/bgp/d2distributed/D2D.java
@@ -1,11 +1,16 @@
 package bgp.d2distributed;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -13,7 +18,6 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class D2D {
@@ -25,36 +29,55 @@ public class D2D {
 	 * VALUEOUT: prodotto delle occurrenze Si*Qi
 	 */
 
-	public static class PartialScoreMapper extends Mapper<Object, Text[], Text, LongWritable> {
+	public static class PartialScoreMapper extends Mapper<Object, Text, Text, LongWritable> {
+		
+		private HashMap<String, Long> Mapfile1 = new HashMap<String, Long>();
+		private HashMap<String, Long> Mapfile2 = new HashMap<String, Long>();
+		
+		@Override
+		protected void setup(Context context) throws IOException, InterruptedException {
+			Path path1 = new Path("hdfs:/user/user/INPUT/seq0_all.res");
+			Path path2 = new Path("hdfs:/user/user/INPUT/seq1_all.res");
+			FileSystem fs = FileSystem.get(new Configuration());
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(fs.open(path1)));
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(fs.open(path2)));
+			Mapfile1 = new HashMap<String, Long>();
+			Mapfile2 = new HashMap<String, Long>();
+			String kmerS="", kmerQ="";
+			String line1 = "",line2 = "";
+			long occS=0, occQ=0;
+			String[] arr1, arr2;
+			while((line1 = br1.readLine()) != null && !line1.equals("")) {
+				arr1=line1.split("\t");
+				System.out.println("arr1[0]:"+arr1[0]+ " arr1[1]:" + arr1[1]);
+				occS = Long.valueOf(arr1[0]);
+				kmerS = arr1[1];			
+				Mapfile1.put(kmerS, occS);
+			}
+			while((line2 = br2.readLine()) != null && !line2.equals("")) {
+				arr2=line2.split("\t");
+				System.out.println("arr2[0]:"+arr2[0]+ " arr2[1]:" + arr2[1]);
+				occQ = Long.valueOf(arr2[0]);
+				kmerQ = arr2[1];
+				Mapfile2.put(kmerQ, occQ);
+			}
+			System.out.println("SETUP COMPLETE");
+		}
 
 		@Override
-		public void map(Object key, Text[] value, Context context) throws IOException, InterruptedException {
-			StringTokenizer stTokFile1 = new StringTokenizer(value[0].toString());
-			StringTokenizer stTokFile2 = new StringTokenizer(value[1].toString());
-			String kmerS="", kmerQ="";
-			long Si=0, Qi=0;
-			HashMap<String, Long> file1 = new HashMap<String, Long>();
-			HashMap<String, Long> file2 = new HashMap<String, Long>();
-			while(stTokFile1.hasMoreTokens() && stTokFile2.hasMoreTokens()) {
-				kmerS = stTokFile1.nextToken();
-				kmerQ = stTokFile2.nextToken();
-				Si = Long.valueOf(stTokFile1.nextToken());
-				Qi = Long.valueOf(stTokFile2.nextToken());
-				file1.put(kmerS, Si);
-				file2.put(kmerQ, Qi);
-			}
-			Iterator<Entry<String,Long>> it1 = file1.entrySet().iterator();
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			System.out.println("START MAP \n value: "+value.toString());
+			Iterator<Entry<String,Long>> it1 = Mapfile1.entrySet().iterator();
 			Entry<String, Long> currEntry = null;
 			String currKmer = "";
-			long currOcc1 = 0;
-			long currOcc2 = 0;
+			long currOcc1 = 0, currOcc2 = 0;
 			Long partialScore = new Long(0); 
 			while(it1.hasNext()){
 				currEntry = it1.next();
 				currKmer = currEntry.getKey();
 				currOcc1 = currEntry.getValue();
-				if(file2.containsKey(currKmer)) {
-					currOcc2 = file2.get(currKmer);
+				if(Mapfile2.containsKey(currKmer)) {
+					currOcc2 = Mapfile2.get(currKmer);
 					partialScore = currOcc1*currOcc2;
 					context.write(new Text(currKmer), new LongWritable(partialScore));
 				}
@@ -72,30 +95,34 @@ public class D2D {
 		@Override
 		protected void reduce(Text kmer, Iterable<LongWritable> partialScore, Context context) throws IOException, InterruptedException {
 			long score = 0;
+			int count = 0;
 			for (LongWritable pScore : partialScore) {
+				System.out.println("Iterazione num: "+ ++count);
 				score += pScore.get();
+				System.out.println("pScore: "+ pScore.get() + " score: "+ score);
 			}
+			System.out.println("END REDUCER: \n score: "+score);
 			this.result.set(score);
-			context.write(kmer,this.result);
+			context.write("Score",this.result);
 		}
 	}
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
-		System.out.println("Configuration conf = new Configuration();");
 		Job job = Job.getInstance(conf, "d2d");
-		System.out.println("Job job = Job.getInstance(conf, \"d2d\");");
-		job.setJarByClass(D2D.class);
-		System.out.println("job.setJarByClass(D2D.class);");
-		job.setMapperClass(PartialScoreMapper.class);
-		job.setReducerClass(D2ScoreReducer.class);
-		job.setOutputKeyClass(Object.class);
-		job.setOutputValueClass(LongWritable.class);
-		NLineInputFormat.setNumLinesPerSplit(job, 101);
+		job.setJarByClass(D2D.class); //setta la classe del programma
+		job.setMapperClass(PartialScoreMapper.class); //setta la classe Mapper
+		job.setReducerClass(D2ScoreReducer.class); //setta la classe Reducer
+		job.setOutputKeyClass(Object.class); //setta la key class del reducer
+		job.setOutputValueClass(LongWritable.class); //setta la value class del reducer
+		job.setMapOutputKeyClass(Text.class); //setta la key class output del mapper
+		job.setMapOutputValueClass(LongWritable.class); //setta la value class output del mapper
+		job.setInputFormatClass(LineInputFormat.class);
+		System.out.println("Num reduce task: " + job.getNumReduceTasks());
+		job.setNumReduceTasks(1);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
-
 
 }
---
+++ distributed/d2d/src/main/java/bgp/d2distributed/LineInputFormat.java
@@ -0,0 +1,96 @@
+package bgp.d2distributed;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
+import org.apache.hadoop.util.LineReader;
+
+public class LineInputFormat extends FileInputFormat<LongWritable, Text> {
+
+		  public RecordReader<LongWritable, Text> createRecordReader(InputSplit genericSplit, TaskAttemptContext context) throws IOException {
+		    context.setStatus(genericSplit.toString());
+		    return new LineRecordReader();
+		  }
+
+		  /** 
+		   * Logically splits the set of input files for the job, splits N lines
+		   * of the input as one split.
+		   * 
+		   * @see FileInputFormat#getSplits(JobContext)
+		   */
+		  public List<InputSplit> getSplits(JobContext job)
+		  throws IOException {
+		    List<InputSplit> splits = new ArrayList<InputSplit>();
+		    for (FileStatus status : listStatus(job)) {
+		      splits.addAll(getSplitsForFile(status,job.getConfiguration()));
+		    }
+		    return splits;
+		  }
+		  
+		  public static List<FileSplit> getSplitsForFile(FileStatus status, Configuration conf) throws IOException {
+		    List<FileSplit> splits = new ArrayList<FileSplit> ();
+		    Path fileName = status.getPath();
+		    if (status.isDirectory()) {
+		      throw new IOException("Not a file: " + fileName);
+		    }
+		    FileSystem  fs = fileName.getFileSystem(conf);
+		    LineReader lr = null;
+		    try {
+		      FSDataInputStream in  = fs.open(fileName);
+		      lr = new LineReader(in, conf);
+		      Text line = new Text();
+		      Text lineVoid = new Text("");
+		      long begin = 0;
+		      long length = 0;
+		      int num = -1;
+		      while ((num = lr.readLine(line)) > 0) {
+		        length += num;    
+		        if(line.equals(lineVoid)) {
+		        	splits.add(createFileSplit(fileName, begin, length));
+			        begin += length;
+			        length = 0;
+		        }
+		      }
+		      /*if (numLines != 0) {
+		        splits.add(createFileSplit(fileName, begin, length));
+		      }*/
+		    } finally {
+		      if (lr != null) {
+		        lr.close();
+		      }
+		    }
+		    return splits; 
+		  }
+
+		  /**
+		   * NLineInputFormat uses LineRecordReader, which always reads
+		   * (and consumes) at least one character out of its upper split
+		   * boundary. So to make sure that each mapper gets N lines, we
+		   * move back the upper split limits of each split 
+		   * by one character here.
+		   * @param fileName  Path of file
+		   * @param begin  the position of the first byte in the file to process
+		   * @param length  number of bytes in InputSplit
+		   * @return  FileSplit
+		   */
+		  protected static FileSplit createFileSplit(Path fileName, long begin, long length) {
+		    return (begin == 0) 
+		    ? new FileSplit(fileName, begin, length - 1, new String[] {})
+		    : new FileSplit(fileName, begin - 1, length, new String[] {});
+		  }
+		  
+}
