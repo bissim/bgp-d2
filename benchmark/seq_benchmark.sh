90379f6d31526c606778ce76802b69cb4329627a
Simone B <s.bisogno10@studenti.unisa.it>
2019-07-31 12:43:53 +0200

    Update benchmark BASH script It runs the SequentialBenchmark class from D2 project.

--- benchmark/seq_benchmark.sh
+++ benchmark/seq_benchmark.sh
@@ -1,19 +1,17 @@
+# Runs sequential benchmark for D2 algorithm by
+# using custom-defined parameters.
 
 OUT_FILE=out
 
-MIN_LENGTH_SEQ=100
-MAX_LENGTH_SEQ=1000
 NUM_SEQ=10
-DATABASE_NAME=benchsuite
-OUTDIR=tmp_suite
+INDIR=sequences
 TOP_LINES_OCCURANCES=1000
 
 echo "Sequential Benchmark" >> $OUT_FILE
 date >> $OUT_FILE
 
 echo "Run SequentialD2" >> $OUT_FILE
-java -classpath d2-0.0.1.jar generator.MainGeneratorMultipleFiles $OUTDIR $TOP_LINES_OCCURANCES yes >> $OUT_FILE
+java -classpath d2-*.jar benchmark.SequentialBenchmark $INDIR $NUM_SEQ $TOP_LINES_OCCURANCES yes >> $OUT_FILE
 echo "DONE" >> $OUT_FILE
-
 
 echo "----------------------------" >> $OUT_FILE
